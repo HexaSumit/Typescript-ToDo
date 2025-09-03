187d209defcf77f7bf36b213b4548eeffbd84f3e
Sumit Bhardwaj <sumitbhardwaj78368@gmail.com>
2025-09-03 20:03:43 +0530

    feat: add delete task functionality

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,28 +1,46 @@
+import type { Dispatch, SetStateAction } from "react"
 import type { todoType } from "../types/TodoTypes"
 
-type TodoListProp={
-    todos:todoType[]
+type TodoListProp = {
+    todos: todoType[]
+    setTodos: Dispatch<SetStateAction<todoType[]>>
 }
-const TodoList = ({todos}:TodoListProp) => {
-  return (
-    <div className="w-[50%] flex flex-col gap-2">
-      {todos.length === 0 ? (
-        <p className="text-gray-500">No todos yet!</p>
-      ) : (
-        todos.map((todo) => (
-          <div
-            key={todo.id}
-            className="flex justify-between items-center bg-white shadow p-2 rounded"
-          >
-            <span className={todo.completed ? "line-through text-gray-500" : ""}>
-              {todo.task}
-            </span>
-            <span>{todo.completed ? "✅" : "⏳"}</span>
-          </div>
-        ))
-      )}
-    </div>
-  )
+const TodoList = ({ todos, setTodos }: TodoListProp) => {
+
+    const handleDelete = (id: number) => {
+        setTodos((prev) => prev.filter((todo) => todo.id !== id));
+    };
+
+    return (
+        <div className="w-[50%] flex flex-col gap-2">
+            {todos.length === 0 ? (
+                <p className="text-gray-500 text-lg p-5">No todos yet!</p>
+            ) : (
+                todos.map((todo) => (
+                    <div
+                        key={todo.id}
+                        className="flex justify-between items-center bg-white shadow p-2 rounded"
+                    >
+                        <span className={todo.completed ? "line-through text-gray-500" : ""}>
+                            {todo.task}
+                        </span>
+                        <div className="flex items-center gap-2 cursor-pointer">
+                            <span>{todo.completed ? "✅" : "⏳"}</span>
+
+                            {/* Delete Button */}
+                            <button
+                                className="bg-red-500 hover:bg-red-600 text-white px-2 py-1 rounded"
+                                onClick={() => handleDelete(todo.id)}
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                ))
+            )}
+        </div>
+
+    )
 }
 
 export default TodoList
