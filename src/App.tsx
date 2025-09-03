8dd63af9a490787374cfcfa80ddd19197ff45e71
Sumit Bhardwaj <sumitbhardwaj78368@gmail.com>
2025-09-03 19:18:12 +0530

    feat(app): setup base layout with static todos and integrate TodoInput & TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
+import { useState } from 'react'
 import './App.css'
 import Todoinput from './components/Todoinput'
+import TodoList from './components/TodoList'
+import type { todoType } from './types/TodoTypes'
 
 function App() {
+  const [todos, setTodos] = useState<todoType[]>([
+    {
+      id: 1,
+      task: 'first todo',
+      completed: true,
+    },
+    {
+      id: 2,
+      task: 'sec todo',
+      completed: false,
+    },
+  ])
   return (
     <>
-      <div className=' min-h-screen bg-gray-500'>
-        <div className=' flex items-center justify-center p-5 mb-4'>
-          <h2 className=' text-2xl font-semibold underline'>Your Todo App</h2>
-        </div>
-        <Todoinput />
+      <div className="min-h-screen bg-gray-50 flex flex-col items-center p-6">
+        <h1 className="text-2xl font-bold mb-6">My Todo App</h1>
+
+        {/* Input Box */}
+        < Todoinput />
+
+        {/* Todo List */}
+        <TodoList todos={todos} />
       </div>
     </>
   )
 }
 
 export default App
+  
+  
