54dd0ebae2e2002d4bdc6192b6991e6f56f3a31c
Sumit Bhardwaj <sumitbhardwaj78368@gmail.com>
2025-09-03 20:05:48 +0530

    feat(todo): implement adding task from input

--- src/components/Todoinput.tsx
+++ src/components/Todoinput.tsx
@@ -1,9 +1,29 @@
-import { useState, type FormEvent } from "react"
+import { useState, type Dispatch, type FormEvent, type SetStateAction } from "react"
+import type { todoType } from "../types/TodoTypes"
 
-const Todoinput = () => {
-    const [input,setInput]=useState<string>("")
-    const handlesubmit=(e:FormEvent)=>{
+type TodoinputProps = {
+    setTodos:  Dispatch<SetStateAction<todoType[]>>
+}
+const Todoinput = ({ setTodos }: TodoinputProps) => {
+    const [input, setInput] = useState<string>("")
+
+    const handlesubmit = (e: FormEvent) => {
         e.preventDefault();
+
+        if (!input.trim()) return // empty input check
+
+        // naya todo create
+        const newTodo = {
+            id: Date.now(),
+            task: input,
+            completed: false
+        }
+
+        // **props se setTodos call karna**
+        setTodos(prev => [...prev, newTodo])
+
+        // input clear kar do
+        setInput("")
     }
     return (
         <form className=" w-full flex items-center justify-center" onSubmit={handlesubmit}>
@@ -13,7 +33,7 @@ const Todoinput = () => {
                     placeholder="Enter a new task..."
                     className="flex-1 border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     value={input}
-                    onChange={(e)=>setInput(e.target.value)}
+                    onChange={(e) => setInput(e.target.value)}
                 />
                 <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg shadow">
                     Add
