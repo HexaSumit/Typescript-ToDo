e7b82ebe2aedc230b26402b774e5f2576cacfcf2
Sumit Bhardwaj <sumitbhardwaj78368@gmail.com>
2025-09-03 20:06:25 +0530

    passed props

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,17 @@ import TodoList from './components/TodoList'
 import type { todoType } from './types/TodoTypes'
 
 function App() {
-  const [todos, setTodos] = useState<todoType[]>([
-    {
-      id: 1,
-      task: 'first todo',
-      completed: true,
-    },
-    {
-      id: 2,
-      task: 'sec todo',
-      completed: false,
-    },
-  ])
+  const [todos, setTodos] = useState<todoType[]>([])
   return (
     <>
       <div className="min-h-screen bg-gray-50 flex flex-col items-center p-6">
         <h1 className="text-2xl font-bold mb-6">My Todo App</h1>
 
         {/* Input Box */}
-        < Todoinput />
+        < Todoinput setTodos={setTodos}/>
 
         {/* Todo List */}
-        <TodoList todos={todos} />
+        <TodoList todos={todos} setTodos={setTodos}/>
       </div>
     </>
   )
